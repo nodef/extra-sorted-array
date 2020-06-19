55c2844c8208f097f7a286a22617b37b2c92cf8d
Subhajit Sahu <wolfram77@gmail.com>
2020-06-19 12:49:27 +0530

    :coconut: chutney +bsearch

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {default as search} from './search';
+export {default as searchAny} from './searchAny';
+export {default as searchClosest} from './searchClosest';
+export {default as searchRight} from './searchRight';
